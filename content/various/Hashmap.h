6200a34f96dd9643f60fcc97a0ce06648ecc0dc0
RobeZH <541736690@qq.com>
2023-09-08 23:39:03 -0500

    change hash table

--- content/various/Hashmap.h
+++ content/various/Hashmap.h
@@ -1,19 +1,28 @@
 /**
- * Author: Andrew He
- * Description: Faster/better hash maps, taken from CF
+ * Author: 
+ * Date: 
+ * License: CC0
+ * Source: 
+ * Description: 
+ * Time: Faster/better hash maps, taken from CF
  */
-#pragma once
 
-#include <ext/pb_ds/assoc_container.hpp> /** keep-include */
+#include <ext/pb_ds/assoc_container.hpp> 
 using namespace __gnu_pbds;
 gp_hash_table<int, int> table;
-
 struct custom_hash {
-	size_t operator()(uint64_t x) const {
-		x += 48;
-		x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9;
-		x = (x ^ (x >> 27)) * 0x94d049bb133111eb;
-		return x ^ (x >> 31);
-	}
+    static uint64_t splitmix64(uint64_t x) {
+        // http://xorshift.di.unimi.it/splitmix64.c
+        x += 0x9e3779b97f4a7c15;
+        x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9;
+        x = (x ^ (x >> 27)) * 0x94d049bb133111eb;
+        return x ^ (x >> 31);
+    }
+
+    size_t operator()(uint64_t x) const {
+        static const uint64_t FIXED_RANDOM = chrono::steady_clock::now().time_since_epoch().count();
+        return splitmix64(x + FIXED_RANDOM);
+    }
 };
+
 gp_hash_table<int, int, custom_hash> safe_table;
